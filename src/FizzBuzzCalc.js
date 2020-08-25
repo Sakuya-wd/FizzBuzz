b2497f57a99cdc69c69ecc3944b521946b24596b
Sakuya-wd <yamamoto1.527@gmail.com>
2020-08-25 13:30:27 +0900

    テストを作成、FizzBuzzを修正しました

---
+++ src/FizzBuzzCalc.js
@@ -0,0 +1,78 @@
+
+const FizzBuzzCalc = (num) =>{
+
+  //整数でない時、あるいは1未満または100より大きい時エラーを出力する。
+  if(OutOfRange(num)){
+
+     throw new RangeError("引数が不正です。1~100以内の整数のみ引数に設定できます。");
+
+  }
+
+  //整数が3と5の公倍数の時は、整数の代わりにFizzBuzzを出力する。(この時にFizzとBuzzは出力しない)
+  //そのため3の倍数と5の倍数の処理前に3と5の公倍数の処理を行う。
+  if(isFizzBuzz(num)){
+
+    return `FizzBuzz`;
+
+  }
+
+  //3の倍数の時は、整数の代わりにFizzを出力する
+  if(isFizz(num)){
+
+    return `Fizz`;
+
+  }
+
+  //5の倍数の時は、整数の代わりにBuzzを出力する
+  if(isBuzz(num)){
+
+    return `Buzz`;
+
+  }
+
+  //整数が3の倍数でも5の倍数でもない時、整数をそのまま出力する。
+  return num;
+
+}
+
+
+const OutOfRange = (num) =>{
+
+  if(typeof num !== "number") return true;
+  if(Math.round(num) != num) return true;
+  if(num<1 || 100<num) return true;
+
+  return false;
+
+}
+
+
+const isFizzBuzz = (num) =>{
+
+  if(num%3==0 && num%5==0) return true;
+
+  return  false;
+
+}
+
+
+const isFizz = (num) =>{
+
+  if(num%3==0) return true;
+
+  return false;
+
+}
+
+
+const isBuzz = (num) =>{
+
+  if(num%5==0) return true;
+
+  return false;
+
+}
+
+
+//module.exports = FizzBuzzCalc;
+export {FizzBuzzCalc};
---
+++ test/FizzBuzz.test.js
@@ -0,0 +1,22 @@
+
+const FizzBuzzCalc = require('../src/FizzBuzzCalc.js');
+
+
+describe('FizzBuzzCalc', () =>{
+
+    test('FizzBuzzCalc', ()=>{
+       
+        //例外処理が網羅されているかを確認。
+        expect(() => FizzBuzzCalc(`test`)).toThrow();
+        expect(() => FizzBuzzCalc(-1)).toThrow();
+        expect(() => FizzBuzzCalc(101)).toThrow();
+
+        //例外処理以外の仕様が網羅されているかを確認。
+        expect(FizzBuzzCalc(15)).toBe('FizzBuzz');
+        expect(FizzBuzzCalc(3)).toBe('Fizz');
+        expect(FizzBuzzCalc(5)).toBe('Buzz');
+        expect(FizzBuzzCalc(1)).toBe(1);
+
+    })
+
+})
