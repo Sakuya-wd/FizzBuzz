d20285d438bd8ca6f23355f8329d591c95e20dee
Sakuya-wd <yamamoto1.527@gmail.com>
2020-09-05 11:23:27 +0900

    FizzBuzzを修正しました

--- src/FizzBuzzCalc.js
+++ src/FizzBuzzCalc.js
@@ -1,6 +1,36 @@
 
 const FizzBuzzCalc = (num) =>{
 
+  //整数でない時、あるいは1未満または100より大きい時エラーを出力する。
+  if(OutOfRange(num)){
+
+    throw new RangeError("引数が不正です。1~100以内の整数のみ引数に設定できます。");
+
+  //整数が3と5の公倍数の時は、整数の代わりにFizzBuzzを出力する。(この時にFizzとBuzzは出力しない)
+  //そのため3の倍数と5の倍数の処理前に3と5の公倍数の処理を行う。
+  }else if(isFizzBuzz(num)){
+
+    return `FizzBuzz`;
+
+  //3の倍数の時は、整数の代わりにFizzを出力する
+  }else if(isFizz(num)){
+
+    return `Fizz`;
+
+  //5の倍数の時は、整数の代わりにBuzzを出力する
+  }else if(isBuzz(num)){
+
+    return `Buzz`;
+
+  //整数が3の倍数でも5の倍数でもない時、整数をそのまま出力する。
+  }else{
+
+    return num;
+
+  }
+
+}
+
 
 const OutOfRange = (num) =>{
 
@@ -33,41 +63,6 @@ const isBuzz = (num) =>{
   if(num%5==0) return true;
 
   return false;
-
-}
-
-
-//整数でない時、あるいは1未満または100より大きい時エラーを出力する。
-if(OutOfRange(num)){
-
-  throw new RangeError("引数が不正です。1~100以内の整数のみ引数に設定できます。");
-
-}
-
-//整数が3と5の公倍数の時は、整数の代わりにFizzBuzzを出力する。(この時にFizzとBuzzは出力しない)
-//そのため3の倍数と5の倍数の処理前に3と5の公倍数の処理を行う。
-if(isFizzBuzz(num)){
-
- return `FizzBuzz`;
-
-}
-
-//3の倍数の時は、整数の代わりにFizzを出力する
-if(isFizz(num)){
-
- return `Fizz`;
-
-}
-
-//5の倍数の時は、整数の代わりにBuzzを出力する
-if(isBuzz(num)){
-
- return `Buzz`;
-
-}
-
-//整数が3の倍数でも5の倍数でもない時、整数をそのまま出力する。
-return num;
 
 }
 
